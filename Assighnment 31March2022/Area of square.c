5c87299a620ebe27a8ae591b5cfddb2d05acc4f7
AniketB007 <aniketbourasee007.jit@gmail.com>
2022-04-01 11:30:25 +0530

    Formulas

---
+++ Assighnment 31March2022/Area of square.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	float area,side;
+	printf("Enter the value of side");
+	scanf("%f",&side);
+	area=side*side;
+	printf(" The area of square = %f",area);
+	getch();
+}
