b4d460631bb0cd303ddec8c0e56304d2cfe80c28
AniketB007 <aniketbourasee007.jit@gmail.com>
2022-04-05 23:11:28 +0530

    5 April 2022

---
+++ C programs/Division of two number.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n1,n2,res;
+	printf("Enter two numbers ");
+	scanf("%d %d",&n1,&n2);
+	res=n1/n2;
+	printf("Result after dividing these number is %d",res);
+	
+}
+
