51109d0da21f54b46c6c7643404f95aea86d88b9
AniketB007 <aniketbourasee007.jit@gmail.com>
2022-04-05 22:57:24 +0530

    5 April 2022

---
+++ C programs/Personal details.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	printf("Name   :- Aniket Sunil Bourasee\n");
+	printf("DOB    :- Febuary 28,2001\n");
+	printf("Mobile :- +91 9112319964");
+	getch();
+}
+
