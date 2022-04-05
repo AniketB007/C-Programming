6faa8814c69f577682f8db4db662ab050dd49124
AniketB007 <aniketbourasee007.jit@gmail.com>
2022-04-05 23:33:10 +0530

    5 April 2022

---
+++ C programs/Perimeter and Area of rectangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int height=7,width=5,area,peri_meter;
+		peri_meter=2*(height+width);
+	printf("The perimeter of rectangle = %d\n",peri_meter);
+	area=width*height;
+	printf("The area of rectangle = %d ",area);
+	
+getch();	
+}
