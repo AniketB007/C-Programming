1bfee4ac5b820c4f4cf9a58437ccd5ddf9a7fd4c
AniketB007 <aniketbourasee007.jit@gmail.com>
2022-04-05 23:43:55 +0530

    5 April 2022

---
+++ C programs/Perimeter and Area of  circle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	float area,pi=3.14,peri_meter,r=6;
+	peri_meter=2*pi*r;
+	printf("The perimeter of circle= %f\n",peri_meter);
+	area=(pi*r*r);
+	printf("The area of circle =%f",area);
+	
+getch();	
+}
