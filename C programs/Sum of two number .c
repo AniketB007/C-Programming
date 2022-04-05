3c8dbd7a2cb3843fad35f3163c39ad0746b694d3
AniketB007 <aniketbourasee007.jit@gmail.com>
2022-04-05 22:59:26 +0530

    5 April 2022

---
+++ C programs/Sum of two number .c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n1,n2,sum;
+	printf("Enter any two numbers ");
+	scanf("%d %d",&n1,&n2);
+	sum=n1+n2;
+	printf("The sum of two number is %d",sum);
+	getch();
+	
+}
+
