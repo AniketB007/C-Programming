cb42ad759fb2d20f23f1e0e511f964d1a05998ee
AniketB007 <aniketbourasee007.jit@gmail.com>
2022-04-07 12:09:54 +0530

    Assighnment 01

---
+++ C programs/Number of year,week and days.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int d1,y,w,d2;
+	printf("Enter the number of days :");
+	scanf("%d",&d1);
+	
+	y=d1/365;
+	printf("The number of years : %d\n",y);
+	
+	w=(d1%365)/7;
+	printf("Number of weeks : %d\n",w);
+	
+	d2=d1-((y*365)+(w*7));
+	printf("The number of days = %d\n",d2);
+}
+
+
