dff1c5bd0344a6575c72b4c6df45b80ecee8d23c
AniketB007 <101799278+AniketB007@users.noreply.github.com>
2022-04-10 14:10:26 +0530

    Update Correct or Wrong.c

--- C programs/Correct or Wrong.c
+++ C programs/Correct or Wrong.c
@@ -15,7 +15,7 @@ scanf("%d",&r);
 printf("Enter the value of 4th integer");
 scanf("%d",&s);
 
-if((r>0)&&(s>0)&&(p%2==0)&&(q>r)&&(s>p)&&((r+s)==(p=q)))
+if((r>0)&&(s>0)&&(p%2==0)&&(q>r)&&(s>p)&&((r+s)>(p+q)))
 {
 	printf("The values are Correct");
 	
