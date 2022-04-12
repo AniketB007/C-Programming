b5899103013098eca114e29034b150d3d33c54b9
AniketB007 <aniketbourasee007.jit@gmail.com>
2022-04-13 00:31:00 +0530

    Assighnment 04

---
+++ C programs/Ascending and Decending.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b;
+	printf("Enter the 1st number");
+	scanf("%d",&a);
+	printf("Enter the 2nd number");
+	scanf("%d",&b);
+	if(b>a)
+	
+	{
+		printf("The numbers are in Ascending Order");
+	}
+	else
+	{
+		printf("The numbers are in Decending order");
+	}
+}
