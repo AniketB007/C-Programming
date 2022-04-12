88deb5d3735f52a2e646740293be5dc9e7c9177d
AniketB007 <aniketbourasee007.jit@gmail.com>
2022-04-13 00:12:04 +0530

    Fibnocci series

---
+++ C programs/fibnocci series.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int r,a,b,c; 
+a=0;
+b=1;
+printf("Enter r");
+scanf("%d",&r);
+printf("%d\n %d\n",a,b);
+c=0;
+while(c<=r)
+{
+	c=a+b;
+	if(c<=r)
+	{
+		printf("%d\n",c);
+		
+	}
+	a=b;
+	b=c;
+	
+}
+
+getch();	
+}
