50e66fcefc19f5648c04578a676788718e3a85ac
AniketB007 <101799278+AniketB007@users.noreply.github.com>
2022-04-18 23:11:37 +0530

    Update print square.c

--- C programs/print square.c
+++ C programs/print square.c
@@ -6,12 +6,12 @@ int main()
 	printf("Enter the number upto which square required");
 	scanf("%d",&n);
 	printf("\nThe square of even value from 1 to %d\n",n);
-    i=1;
+    i=2;
 	while(i<=n)
 	{
 	if (i%2==0)
 	{
-		printf("%d\n",i);
+		printf("%d^2 = %d\n",i,i*i);
 	}
 	i++;
 	}
