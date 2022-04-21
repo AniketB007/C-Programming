bb35e94c1ef420d5127d773308b0972575fafa40
AniketB007 <101799278+AniketB007@users.noreply.github.com>
2022-04-21 23:55:51 +0530

    patterns

---
+++ C programs/pattern 12.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int r,c,n;
+	for (r=1;r<=1;r++)
+	{
+		for (c=5;c>=1;c++)
+		{
+			if(c<=r)
+			printf("%d",c);
+			else
+			printf("*");
+		}
+		printf("\n");
+	}
+}
