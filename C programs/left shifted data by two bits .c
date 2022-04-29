e7bb0034a6622a3dec3faaf10341202f5e1757ae
AniketB007 <101799278+AniketB007@users.noreply.github.com>
2022-04-29 12:08:53 +0530

    Assighnment Q52

---
+++ C programs/left shifted data by two bits .c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n1,n2;
+	printf("Enter the number");
+	scanf("%d",&n1);
+	n1<<=2;
+	n2=n1;
+	printf("The left shifted data is %d",n2);
+	
+	
+ }                                                                                  
