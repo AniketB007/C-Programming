b3534252da06208279182a21d4a2e324ed0aab01
AniketB007 <101799278+AniketB007@users.noreply.github.com>
2022-05-01 18:33:39 +0530

    Multiplication of three numbers.c

---
+++ C programs/Multiplication of three numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n1,n2,n3,M;
+	printf("Enter the 1st Number ");
+	scanf("%d",&n1);
+	printf("Enter the 2nd Number  ");
+	scanf("%d",&n2);
+	printf("Enter the 3rd Number  ");
+	scanf("%d",&n3);
+	M=n1*n2*n3;
+	printf("\nMultiplication of these numbers = %d\n",M);
+	
+}
