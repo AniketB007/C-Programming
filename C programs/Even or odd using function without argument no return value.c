995c27dbf150964ea43525be396ca32864c005e8
AniketB007 <101799278+AniketB007@users.noreply.github.com>
2022-05-04 13:17:19 +0530

    function type1

---
+++ C programs/Even or odd using function without argument no return value.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void check();
+void main()
+{
+	check();
+}
+ check()
+
+{
+	int n1,n;
+	printf("Enter the number");
+	scanf("%d",&n1);
+	n=n1%2;
+	if(n==0)
+	{
+		printf("The given number is Even");
+	}
+	else
+	{
+		printf("The given number is odd");
+	}
+}
