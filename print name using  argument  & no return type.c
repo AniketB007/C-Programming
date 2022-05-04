9b008445fb4ef774cb4acc6ccb00350e5a593fd5
AniketB007 <101799278+AniketB007@users.noreply.github.com>
2022-05-04 12:36:03 +0530

    function type 02

---
+++ print name using  argument  & no return type.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	name();
+}
+
+ void name()
+ {
+ 
+	int i;
+    print(i);
+}
+void print(int j)
+{
+		for(j=1;j<=100;j++)
+	{
+		printf("Aniket\n");
+		
+	}
+}
