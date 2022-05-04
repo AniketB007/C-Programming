493728926e38e261c7c80b46e034e9a4abdead67
AniketB007 <101799278+AniketB007@users.noreply.github.com>
2022-05-04 12:48:09 +0530

    Function type 1

---
+++ C programs/Simple Interest using Function without argument and no return value.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void SI();
+void main()
+{
+	SI();
+}
+
+void SI()
+{
+	float p,n,r,SI;
+	printf("Enter the principal amount");
+	scanf("%f",&p);
+	printf("Enter the number of days");
+	scanf("%f",&n);
+	printf("Enter the rate of intrest");
+	scanf("%f",&r);
+	SI= (p*n*r)/100;
+	printf(" The simple Intrest = %f",SI);
+	getch();
+}
