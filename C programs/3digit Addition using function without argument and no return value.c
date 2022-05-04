7ceb80cd18d69574b3bc8f2ca2bcd0228f1f3ed7
AniketB007 <101799278+AniketB007@users.noreply.github.com>
2022-05-04 13:08:08 +0530

    function type 1

---
+++ C programs/3digit Addition using function without argument and no return value.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void add();
+void main()
+{
+  add();
+}
+void add()
+{
+	int num,n1,n2,n3,total;
+	printf("Enter the 3 digit number");
+	scanf("%d",&num);
+	n1=num%10;
+	num=num/10;
+	n2=num%10;
+	num=num/10;
+	n3=num%10;
+	total=n1+n2+n3;
+	printf("The total of Number = %d",total);
+		
+}
