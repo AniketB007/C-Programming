38a3f4d52acb0ac5b654cc4d2ae1e0ec5f6ae06a
AniketB007 <101799278+AniketB007@users.noreply.github.com>
2022-05-15 23:27:04 +0530

    Assighnment  111

---
+++ C programs/wap to check sum of two given array find which is largest sum .c
@@ -0,0 +1,40 @@
+// Write a C program to compute the sum of the two given arrays of integers,length 3 and find the array which has the largest sum
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n1[3],n2[3],s1=0,s2=0;
+	printf("Enter the 1st array  \n");
+	for(int i=0;i<3;i++)
+	{
+		printf("Enter the element ");
+		scanf("%d",&n1[i]);
+		s1=s1+n1[i];
+	}
+	
+	
+	printf("\n");
+	printf("Enter the 2nd array  \n");
+	for(int i=0;i<3;i++)
+	{
+		printf("Enter the element ");
+		scanf("%d",&n2[i]);
+		s2=s2+n2[i];
+	}
+	if(s1>s2)
+	{
+		printf("Largest sum of array is  \n");
+		for(int i=0;i<3;i++)
+		{
+		printf("%d ",n1[i]);
+		}	
+	}
+	else
+	{
+		printf("The largest sum of  array is  \n");
+		for(int i=0;i<3;i++)
+		{
+			printf(" %d ",n2[i]);
+		}		
+	}	
+}
