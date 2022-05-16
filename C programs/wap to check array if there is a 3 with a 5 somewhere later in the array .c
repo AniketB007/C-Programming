fe3c82f18b89e9642d31afc9181682d3c16189ca
AniketB007 <101799278+AniketB007@users.noreply.github.com>
2022-05-16 10:18:33 +0530

    Assighnment 128

---
+++ C programs/wap to check array if there is a 3 with a 5 somewhere later in the array .c
@@ -0,0 +1,40 @@
+
+   
+/*Write a C program to check a given array of integers and return true if there is a 3 with 
+a 5 somewhere later in the given array.*/
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,i,s=0,j=0;
+    printf("Enter the size of an array ");
+    scanf("%d",&n);
+    int a[n];
+    printf("Enter the element of array  \n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for (i=0;i<n;i++)
+    {
+        if (j && a[i] == 5) 
+        {
+            s=1;
+        }
+        else 
+		if(a[i]==3)
+        {
+            j=1;
+        }
+    }
+    
+    if(s==1)
+    {
+        printf("TRUE");
+    }
+    else
+    {
+        printf("FALSE");
+    }
+}
