17ba56d8ef6419c29f3d3237f293c606d36167e2
AniketB007 <101799278+AniketB007@users.noreply.github.com>
2022-05-16 10:30:17 +0530

    Assighnment 130

---
+++ C programs/wap to check if value 5 appears 5 times and there are  no next to each other  .c
@@ -0,0 +1,39 @@
+/*Write a C program to check a given array of integers and return true if 
+the value 5 appears 5 times and there are no 5 next to each other.*/
+
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n,i,n1=0,n2=0;
+    printf("Enter the size of an array ");
+    scanf("%d",&n);
+    int a[n];
+    printf("Enter the  element of array \n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for (i=0;i<n;i++)
+    {
+        if (a[i] == 5 && !n1) 
+        {
+            n2=n2+1;
+            n1=1;
+        }
+        else
+        {
+            n1=0;
+        }
+    }
+    
+    if(n2==5)
+    {
+        printf("TRUE");
+    }
+    else
+    {
+        printf("FALSE");
+    }
+}
