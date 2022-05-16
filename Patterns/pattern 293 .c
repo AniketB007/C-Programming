8563ea6bb41afb3660f13da7e262a82b3d147eba
AniketB007 <101799278+AniketB007@users.noreply.github.com>
2022-05-16 12:59:17 +0530

    293

---
+++ Patterns/pattern 293 .c
@@ -0,0 +1,26 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int r,c,add=0;
+	for(r=5;r>=1;r--)
+	{
+		add=add+1;
+		for(c=1;c<=r;c++)
+		{	
+			printf("%c",(add+64));
+		}
+		printf("\n");
+	}
+	
+}
+
+/*
+
+AAAAA
+BBBB
+CCC
+DD
+E
+*/
