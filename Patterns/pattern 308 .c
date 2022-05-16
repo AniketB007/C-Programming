3e2e28dd45db694122e1258d7585525e7acdd808
AniketB007 <101799278+AniketB007@users.noreply.github.com>
2022-05-16 13:55:03 +0530

    308

---
+++ Patterns/pattern 308 .c
@@ -0,0 +1,33 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int r,c,add=65;
+	for(r=1;r<=4;r++)
+	{
+		for(c=1;c<=4;c++)
+		{
+			if(r+c>4)
+			{
+				printf("%c ",add);
+				
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+		add=add+2;
+		printf("\n");
+	}
+}
+
+/*
+
+   A
+  C C
+ E E E
+G G G G
+
+*/
