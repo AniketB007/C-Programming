1b9b2dcc4854939e12981716beb0c18817473e9a
AniketB007 <101799278+AniketB007@users.noreply.github.com>
2022-05-16 13:04:36 +0530

    295

---
+++ Patterns/pattern 295 .c
@@ -0,0 +1,24 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int r,c;
+	for(r=5;r>=1;r--)
+	{
+		for(c=1;c<=r;c++)
+		{	
+			printf("%c",(r+64));
+		}
+		printf("\n");
+	}
+	
+}
+
+/*
+EEEEE
+DDDD
+CCC
+BB
+A
+*/
